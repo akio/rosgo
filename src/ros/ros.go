367ebe8ca4a8da6d3a8961202c177256b64942ba
Dave Hershberger <dave.hershberger@sri.com>
2014-08-30 14:09:20 -0700

    Added connect and disconnect callbacks for publishers.

--- src/ros/ros.go
+++ src/ros/ros.go
@@ -1,8 +1,14 @@
 package ros
 
-
 type Node interface {
     NewPublisher(topic string, msgType MessageType) Publisher
+    // Create a publisher which gives you callbacks when subscribers
+    // connect and disconnect.  The callbacks are called in their own
+    // goroutines, so they don't need to return immediately to let the
+    // connection proceed.
+    NewPublisherWithCallbacks(topic string,
+        msgType MessageType,
+        connectCallback, disconnectCallback func(SingleSubscriberPublisher)) Publisher
     NewSubscriber(topic string, msgType MessageType, callback interface{}) Subscriber
     NewServiceClient(service string, srvType ServiceType) ServiceClient
     NewServiceServer(service string, srvType ServiceType, callback interface{}) ServiceServer
@@ -21,36 +27,38 @@ type Node interface {
     Logger() Logger
 }
 
-
 func NewNode(name string) Node {
     return newDefaultNode(name)
 }
-
 
 type Publisher interface {
     Publish(msg Message)
     Shutdown()
 }
 
+// A publisher which only sends to one specific subscriber.  This is
+// sent as an argument to the connect and disconnect callback
+// functions passed to Node.NewPublisherWithCallbacks().
+type SingleSubscriberPublisher interface {
+    Publish(msg Message)
+    GetSubscriberName() string
+    GetTopic() string
+}
 
 type Subscriber interface {
     Shutdown()
 }
 
-
-type ServiceHandler interface {}
-
+type ServiceHandler interface{}
 
 type ServiceFactory interface {
     Name() string
     MD5Sum() string
 }
 
-
 type ServiceServer interface {
     Shutdown()
 }
-
 
 type ServiceClient interface {
     Call(srv Service) error
