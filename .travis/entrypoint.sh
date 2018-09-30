34522ecd0d64f81b24270824f4de8eda7dcdf074
akio <akio.code@gmail.com>
2018-09-30 15:45:22 -0700

    WIP

--- .travis/entrypoint.sh
+++ .travis/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /opt/ros/kinetic/setup.bash
-
-export PATH=$PWD/bin:/usr/local/bin:$PATH
+export PATH=$PWD/bin:/usr/local/go/bin:$PATH
+export GOPATH=$PWD:/usr/local/go
 
 go install github.com/akio/rosgo/gengo
 go generate github.com/akio/rosgo/test/test_message
