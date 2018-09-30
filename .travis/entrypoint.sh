f5bf362c6e259972a9be20d256ed6a75c63306b3
akio <akio.code@gmail.com>
2018-09-30 15:03:33 -0700

    WIP

---
+++ .travis/entrypoint.sh
@@ -0,0 +1,12 @@
+source /opt/ros/kinetic/setup.bash
+
+export PATH=$PWD/bin:$PATH
+
+go install github.com/akio/rosgo/gengo
+go generate github.com/akio/rosgo/test/test_message
+go test github.com/akio/rosgo/xmlrpc
+go test github.com/akio/rosgo/xmlrpc
+go test github.com/akio/rosgo/ros
+go install github.com/akio/rosgo/test/test_message
+go test github.com/akio/rosgo/test/test_message
+
