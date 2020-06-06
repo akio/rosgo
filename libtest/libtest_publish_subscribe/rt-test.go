f24148ae21a09e411c9d84e4cf2a1914ab81580f
Edwin Hayes <edwin.hayes@rocos.io>
2020-06-06 12:35:52 +1200

    Fixed incorrectly handling of namespaces in node creation.

--- libtest/libtest_publish_subscribe/rt-test.go
+++ libtest/libtest_publish_subscribe/rt-test.go
@@ -1,10 +1,11 @@
 package libtest_publish_subscribe
 
 import (
-	"github.com/edwinhayes/rosgo/libtest/msgs/std_msgs"
-	"github.com/edwinhayes/rosgo/ros"
 	"os"
 	"testing"
+
+	"github.com/edwinhayes/rosgo/libtest/msgs/std_msgs"
+	"github.com/edwinhayes/rosgo/ros"
 )
 
 var message string
@@ -40,8 +41,75 @@ func onDisconnect(pub ros.SingleSubscriberPublisher) {
 // RTTest performs a run-time test of using rosgo to create publisher and subscriber nodes
 // The test must remove publishers and subscribers and reinitialize them with same name to pass
 func RTTest(t *testing.T) {
+	rTTest_NoNamespace(t)
+	rTTest_Namespace(t)
+
+	return
+}
+
+func rTTest_NoNamespace(t *testing.T) {
 	// Instantiate a ROS node.
 	node, err := ros.NewNode("/rosgo", os.Args)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer node.Shutdown()
+
+	subscription = 1
+
+	for node.OK() {
+		_ = node.SpinOnce()
+
+		// Try to publish a message.
+		if subscription == 1 {
+			pub, err := node.NewPublisherWithCallbacks("rosgomessage", std_msgs.MsgString, onConnect, onDisconnect)
+			if err != nil {
+				t.Error("NewPublisher failed; ", pub, err)
+				return
+			}
+		} else {
+			m.Data = "Second Subscriber"
+			pub, err := node.NewPublisher("rosgomessage", std_msgs.MsgString)
+			if err != nil {
+				t.Error("NewPublisher failed; ", pub, err)
+				return
+			}
+			pub.Publish(&m)
+		}
+
+		//Try to subscribe to the message
+		node.NewSubscriber("rosgomessage", std_msgs.MsgString, callback)
+
+		//Check the message is the same as what we published
+		if message != "" {
+			if message == "First Subscriber" {
+				//Correct message recieved
+				//Shutdown publisher and subscriber and initiate second test
+				node.RemoveSubscriber("rosgomessage")
+				node.RemovePublisher("rosgomessage")
+
+			} else if message == "Second Subscriber" {
+				//Second subscription worked
+				if eventname == "/rosgo" {
+					return
+				}
+				t.Error("Wrong message event", eventname)
+				return
+			} else {
+				//An incorrect message has been recieved
+				t.Error("Failed callback", message)
+				return
+			}
+		}
+	}
+	t.Error("Node test failed")
+	return
+}
+
+func rTTest_Namespace(t *testing.T) {
+	// Instantiate a ROS node.
+	node, err := ros.NewNode("/ns/rosgo", os.Args)
 	if err != nil {
 		t.Error(err)
 		return
